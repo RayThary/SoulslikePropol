f436ec8790630991d8190f50eb783905bc038a8e
RAYTHARY\ghfjt <ghfjtorl121291@naver.com>
2024-11-04 11:28:29 +0900

    에셋

---
+++ Assets/missingDestroy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class missingDestroy : MonoBehaviour
+{
+
+    
+    public GameObject Unit;
+
+    private Transform[] _prefabCount = new Transform[] { };
+
+    void Start()
+    {
+        
+        _prefabCount = Unit.transform.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < _prefabCount.Length; i++) 
+        {
+            GameObjectUtility.RemoveMonoBehavioursWithMissingScript(_prefabCount[i].gameObject);
+        }
+        
+    }
+
+
+}
